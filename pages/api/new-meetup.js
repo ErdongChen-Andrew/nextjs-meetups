71d97d1e52d3b99d3f77f968dbc8711618c6129a
Erdong Chen <erdong1993@gmail.com>
2021-11-15 01:22:59 -0800

    first commit

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin-andrew:9998877asd@cluster0.f0dnz.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    client.close();
+
+    res.status(201).json({message: "meetup inserted!"})
+  }
+};
+
+export default handler;
---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import { MongoClient } from "mongodb";
+import MeetupList from "../components/meetups/MeetupList";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+};
+
+export const getStaticProps = async () => {
+  //fetch data
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin-andrew:9998877asd@cluster0.f0dnz.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => {
+        return {
+          id: meetup._id.toString(),
+          title: meetup.title,
+          address: meetup.address,
+          image: meetup.image,
+        };
+      }),
+    },
+    revalidate: 1,
+  };
+};
+
+export default HomePage;
